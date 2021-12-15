593e37bda96ddb18eed7ab466402fd2090ea425f
xovest <79008917+xovest@users.noreply.github.com>
2021-12-15 18:13:45 +0200

    update

--- index.js
+++ index.js
@@ -33,4 +33,33 @@ app.get("/todos", async (req, res) => {
   }
 })
 
+app.get("/todos/:id", async (req, res) => {
+  try {
+    const { id } = req.params
+    const todo = await pool.query("SELECT * FROM todo WHERE todo_id = $1", [
+      id
+    ])
+
+    res.json(todo.rows[0])
+  } catch (err) {
+    console.error(err.message)
+  }
+})
+
+//update
+app.put("/todos/:id", async (req, res) => {
+  try {
+    const { id } = req.params
+    const { description } = req.body
+    const updateTodo = await pool.query(
+      "UPDATE todo SET description = $1 WHERE todo_id = $2",
+      [description, id]
+    );
+
+    res.json("Todo was updated!")
+  } catch (err) {
+    console.error(err.message)
+  }
+})
+
 app.listen(5000, () => console.log('Server runnin'))
