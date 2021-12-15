da01eb7918cf93e91f41d5bc1424987e189a864f
xovest <79008917+xovest@users.noreply.github.com>
2021-12-15 18:14:26 +0200

    delete

--- index.js
+++ index.js
@@ -62,4 +62,17 @@ app.put("/todos/:id", async (req, res) => {
   }
 })
 
+//delete
+app.delete("/todos/:id", async (req, res) => {
+  try {
+    const { id } = req.params;
+    const deleteTodo = await pool.query("DELETE FROM todo WHERE todo_id = $1", [
+      id
+    ])
+    res.json("Todo was deleted!")
+  } catch (err) {
+    console.log(err.message)
+  }
+})
+
 app.listen(5000, () => console.log('Server runnin'))
