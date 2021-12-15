519a8f7a5d01607e88e92e1c6778301130b33c0c
xovest <79008917+xovest@users.noreply.github.com>
2021-12-15 18:12:30 +0200

    create

--- index.js
+++ index.js
@@ -1,8 +1,24 @@
 const express = require('express')
 const app = express()
 const cors = require('cors')
+const pool = require("./db")
 
 app.use(cors())
 app.use(express.json())
+
+//routing
+app.post("/todos", async (req, res) => {
+  try {
+    const { description } = req.body
+    const newTodo = await pool.query(
+      "INSERT INTO todo (description) VALUES($1) RETURNING *",
+      [description],
+    );
+
+    res.json(newTodo.rows[0])
+  } catch (err) {
+    console.error(err.message)
+  }
+})
 
 app.listen(5000, () => console.log('Server runnin'))
